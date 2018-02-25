ec97d44baad26feaa296d8e7de31ba7ee8de3711
Laura Chan <laurachan205@gmail.com>
2018-02-25 18:29:07 +0000

    Add strike private method

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -32,9 +32,14 @@ describe ('BowlingGame', function(){
       expect(bowlingGame.currentFrameScore).toEqual(0);
     });
     it('rolling two gutter balls changes the frame', function(){
-      expect(bowlingGame.roll(0));
-      expect(bowlingGame.roll(0));
+      bowlingGame.roll(0);
+      bowlingGame.roll(0);
       expect(bowlingGame.currentFrame).toEqual(2);
+    });
+    it('rolling a strike changes the frame', function(){
+      bowlingGame.roll(10);
+      expect(bowlingGame.currentFrame).toEqual(2);
+      expect(bowlingGame.currentFrameScore).toEqual(0);
     });
 
   });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -13,11 +13,20 @@ BowlingGame.prototype = {
   roll: function(number){
     this.currentFrameScore += number;
     this.rollCount += 1;
-    if(this.rollCount === 2){
+    if(number === 10 ){
+      this.strike();
+    } else if(number < 10 && this.rollCount === 2){
       this.frames[this.currentFrame] = this.currentFrameScore;
       this.currentFrame += 1;
       this.currentFrameScore = 0;
       this.rollCount = 0;
     };
+  },
+
+  strike: function(){
+    this.frames[this.currentFrame] = this.currentFrameScore;
+    this.currentFrame += 1;
+    this.currentFrameScore = 0;
+    this.rollCount = 0;
   },
 }
