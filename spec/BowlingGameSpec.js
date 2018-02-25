87602b5f3e05f2b5de93b08b1377c3d7b61fa4b3
Laura Chan <laurachan205@gmail.com>
2018-02-25 18:02:31 +0000

    Add roll function to bowlinggame

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -2,13 +2,24 @@
 
 describe ('BowlingGame', function(){
 
-  var bowlingGame = new BowlingGame();
+  var bowlingGame;
+
+  beforeEach(function(){
+    bowlingGame = new BowlingGame();
+  });
 
   describe ('When initialized', function(){
     it ('has a total score of 0 ', function(){
-      expect(bowlingGame.totalScore).toBe(0);
+      expect(bowlingGame.totalScore).toEqual(0);
     });
+  });
 
+  describe('roll', function(){
+    it('rolling two rolls, with neither a strike or a spare, adds points to the total score', function(){
+      expect(bowlingGame.roll(5));
+      expect(bowlingGame.roll(2));
+      expect(bowlingGame.totalScore).toEqual(7);
+    });
   });
 
 
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -4,3 +4,9 @@ var BowlingGame = function(){
   this.STARTING_SCORE = 0
   this.totalScore = this.STARTING_SCORE
 };
+
+BowlingGame.prototype = {
+  roll: function(number){
+    this.totalScore += number;
+  },
+}
