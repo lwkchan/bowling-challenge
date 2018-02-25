759a952fe6cc75fbae0243d3d4011f2fc45bc773
Laura Chan <laurachan205@gmail.com>
2018-02-25 19:47:40 +0000

    Tidy tests - splitting up 'striking' to its own scenario

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -21,26 +21,39 @@ describe ('BowlingGame', function(){
       expect(bowlingGame.roll(2));
       expect(bowlingGame.totalScore).toEqual(7);
     });
-    it('rolling a strike adds a bonus of the next two rolls', function(){
-      bowlingGame.roll(10);
-      bowlingGame.roll(2);
-      bowlingGame.roll(7);
-      expect(bowlingGame.totalScore).toEqual(28);
-    });
-    it('rolling two strikes in a row adds a bonus of the 2 rolls consecutive from each', function(){
-      bowlingGame.roll(10);
-      bowlingGame.roll(10);
-      bowlingGame.roll(7);
-      bowlingGame.roll(2);
-      expect(bowlingGame.totalScore).toEqual(55);
-    });
-    it('rolling nine strikes in a row adds a bonus of the 2 rolls consecutive from each', function(){
-      for(var i = 0; i < 9; i ++){
+
+    describe('striking', function(){
+      it('rolling a strike adds a bonus of the next two rolls', function(){
         bowlingGame.roll(10);
-        console.log(bowlingGame.totalScore)
-      }
-      expect(bowlingGame.totalScore).toEqual(240);
+        bowlingGame.roll(2);
+        bowlingGame.roll(7);
+        expect(bowlingGame.totalScore).toEqual(28);
+      });
+      it('rolling two strikes in a row adds a bonus of the 2 rolls consecutive from each', function(){
+        bowlingGame.roll(10);
+        bowlingGame.roll(10);
+        bowlingGame.roll(7);
+        bowlingGame.roll(2);
+        expect(bowlingGame.totalScore).toEqual(55);
+      });
+      it('strike bonus works for later on in the game', function(){
+        bowlingGame.roll(7);
+        bowlingGame.roll(2);
+        bowlingGame.roll(10);
+        bowlingGame.roll(10);
+        bowlingGame.roll(5);
+        bowlingGame.roll(2);
+        expect(bowlingGame.totalScore).toEqual(58);
+      });
+      it('rolling nine strikes in a row adds a bonus of the 2 rolls consecutive from each', function(){
+        for(var i = 0; i < 9; i ++){
+          bowlingGame.roll(10);
+          console.log(bowlingGame.totalScore)
+        }
+        expect(bowlingGame.totalScore).toEqual(240);
+      });
     });
+
   });
 
 
